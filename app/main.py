db6417de76affcf5bc6ec3cf73cca6adf19fd2e1
Nazgul82 <antonio.sa.filho@protonmail.com>
2021-03-01 06:25:46 +0000

    logging updated

--- app/main.py
+++ app/main.py
@@ -1,23 +1,14 @@
 from flask import Flask
-from flask_logs import LogSetup
+import logging
 
 app = Flask(__name__)
 
-app.config["LOG_TYPE"] = os.environ.get("LOG_TYPE", "watched")
-app.config["LOG_LEVEL"] = os.environ.get("LOG_LEVEL", "INFO")
-
-# File Logging Setup
-app.config['LOG_DIR'] = os.environ.get("LOG_DIR", "/var/log)
-app.config['APP_LOG_NAME'] = os.environ.get("APP_LOG_NAME", "app.log")
-app.config['WWW_LOG_NAME'] = os.environ.get("WWW_LOG_NAME", "www.log")
-logs = LogSetup()
-logs.init_app(app)
+logging.basicConfig(filename='/var/log/app.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 @app.route("/")
 def hello():
-    current_app.logger.info("Functional")
-    current_app.logger.error("ERROR!!!")
-    current_app.logger.warning("WARNING!!!")
+    app.logger.info('Info level log')
+    app.logger.warning('Warning level log')
     return "Hello, World! - Bitwala challenge"
 
 if __name__ == "__main__":
