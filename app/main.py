9acb76d50dec04899e65c1eb175ded55425b3cf6
Nazgul82 <antonio.sa.filho@protonmail.com>
2021-03-01 06:07:18 +0000

    log file conf updated

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from flask import Flask
+from flask_logs import LogSetup
+
 app = Flask(__name__)
+
+app.config["LOG_TYPE"] = os.environ.get("LOG_TYPE", "watched")
+app.config["LOG_LEVEL"] = os.environ.get("LOG_LEVEL", "INFO")
+
+# File Logging Setup
+app.config['LOG_DIR'] = os.environ.get("LOG_DIR", "/var/log)
+app.config['APP_LOG_NAME'] = os.environ.get("APP_LOG_NAME", "app.log")
+app.config['WWW_LOG_NAME'] = os.environ.get("WWW_LOG_NAME", "www.log")
+logs = LogSetup()
+logs.init_app(app)
 
 @app.route("/")
 def hello():
+    current_app.logger.info("Functional")
+    current_app.logger.error("ERROR!!!")
+    current_app.logger.warning("WARNING!!!")
     return "Hello, World!"
 
 if __name__ == "__main__":
